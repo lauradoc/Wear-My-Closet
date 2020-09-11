279a83bfefd7ef93c574b75c0645b805fe2dcc61
Laura Docherty <laurahdocherty@gmail.com>
2020-09-11 10:31:45 -0500

    card reformatting

--- static/js/myCart.js
+++ static/js/myCart.js
@@ -6,16 +6,22 @@ $.get('/cartjson', (cart) => {
     for (const item of cart) {
         console.log(item)
         const cartDetails = (
-            `<div class="cart-details" id="cart-details-id-${item.id}">
-                <ul class="item-details-table">
-                    <input type="hidden" name="item-id" value="${item.id}">
-                    <input type="hidden" value=${item.user} >
-                    <li><label>Item Name: ${item.item_name}</li>
-                    <li><label>Return Date: <input id="due-date-${item.id}" type="date" name="due-date-${item.id}"></label></li>
-                    <li>Checkout Status: ${item.status}</li>
-                    <input type="radio" onclick="removeFromCart(this.id)" id="${item.id}" name="remove" value="Remove Item">
-                    <label>Remove item</label>
-                </ul>
+            `<div class="card">
+                <div class="row no-gutters" id="cart-details-id-${item.id}">
+                    <div class="col">    
+                        <div class="card-body">
+                            <p class="card-text">
+                                <input type="hidden" name="item-id" value="${item.id}">
+                                <input type="hidden" value=${item.user} >
+                                <p><label>Item Name: ${item.item_name}</p>
+                                <p><label>Return Date: <input id="due-date-${item.id}" type="date" name="due-date-${item.id}"></label></p>
+                                <p>Checkout Status: ${item.status}</p>
+                                <input type="radio" onclick="removeFromCart(this.id)" id="${item.id}" name="remove" value="Remove Item">
+                                <label>Remove item</label>
+                            </p>
+                        </ul>
+                    </div>
+                </div>
             </div>`
         );
         $('#cart-items').append(cartDetails);
@@ -31,12 +37,12 @@ $('#complete-checkout').on('click', (evt) => {
         console.log(checkout_items)
         for (const item of checkout_items) {
             const checkoutItemDetails = 
-                `<div class="checkout-details">
-                <ul>
-                    <li>Item Name: ${item.item_name}</li>
-                    <li>Checkout Date: ${item.checkout_date}</li>
-                    <li>Due Date: ${item.due_date }</li>
-                </ul>
+                `<div class="card">
+                <p class="card-text">
+                    <p>Item Name: ${item.item_name}</p>
+                    <p>Checkout Date: ${item.checkout_date}</p>
+                    <p>Due Date: ${item.due_date }</p>
+                </p>
                 </div>`
             ;
             $('#cart-details-form').empty();
